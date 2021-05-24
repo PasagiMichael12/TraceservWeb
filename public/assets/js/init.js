23ca265f20bbc00d7d4b57ea2db0982c6c49395f
unknown <santosjamillejoynicole@gmail.com>
2021-05-24 14:02:27 +0800

    demo

--- public/assets/js/init.js
+++ public/assets/js/init.js
@@ -1,3 +1,4 @@
 
  //wow js initialization
 new WOW().init();
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       colors:{
         primary:"#7276E8",
         secondary:"#E3F0FC",
+        tertiary:"#000000",
       },
       fontFamily:{
         roboto: ['Roboto']
