ddbd9a9de72409abf447055c81987eb1dd7b6e1a
Maguszxc <Magtalasallon@gmail.com>
2021-05-24 18:28:03 +0800

    Latest Update hero + about

--- public/assets/js/init.js
+++ public/assets/js/init.js
@@ -1,4 +1,16 @@
+//wow js initialization
+new WOW().init()
 
- //wow js initialization
-new WOW().init();
-
+$(document).ready(() => {
+   $('.menu-item').on('click', () => {
+      $('.menu').toggleClass('invisible')
+   })
+   $('#seeMore').on('click', function () {
+      $('.par2').removeClass('hidden')
+      $('#seeMore').addClass('hidden')
+   })
+   $('#seeLess').on('click', function () {
+      $('.par2').addClass('hidden')
+      $('#seeMore').removeClass('hidden')
+   })
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,24 @@
 module.exports = {
-  purge: [],
-  darkMode: false,
-  theme: {
-    extend: {
-      colors:{
-        primary:"#7276E8",
-        secondary:"#E3F0FC",
-        tertiary:"#000000",
+   purge: [],
+   darkMode: false,
+   theme: {
+      extend: {
+         colors: {
+            primary: '#7276E8',
+            secondary: '#E3F0FC',
+            tertiary: '#000000',
+         },
+         fontFamily: {
+            roboto: ['Roboto'],
+            poppins: ['Poppins'],
+         },
+         screens: {
+            xxs: {max: '639px'},
+         },
       },
-      fontFamily:{
-        roboto: ['Roboto']
-      },
-      screens:{
-        'xxs':  {'max': '639px'}
-      },
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+   },
+   variants: {
+      extend: {},
+   },
+   plugins: [],
 }
