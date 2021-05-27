f2b87d7325d51170f27866cf25dd02cc06e486ef
Maguszxc <Magtalasallon@gmail.com>
2021-05-27 18:00:50 +0800

    Tracserv Update 6:00 pm 27/05

---
+++ public/assets/js/MixGallery.js-master/pub/js/index.js
@@ -0,0 +1,43 @@
+mixgallery.render()
+const log = console.log
+
+const elements = [
+   {
+      src: './static/image1.jpg',
+      type: 'img',
+      title: 'Nice Landscape',
+      caption:
+         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id aliquet lectus proin nibh. Diam quam nulla porttitor massa id neque aliquam vestibulum. Velit scelerisque in dictum non. In egestas erat imperdiet sed euismod nisi porta lorem. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Aliquet sagittis id consectetur purus ut. Eget est lorem ipsum dolor sit amet. Iaculis urna id volutpat lacus laoreet non. Enim sit amet venenatis urna. Commodo elit at imperdiet dui accumsan sit amet. Sagittis orci a scelerisque purus semper eget duis. Pharetra pharetra massa massa ultricies mi. Etiam sit amet nisl purus in mollis nunc sed id. Tristique senectus et netus et malesuada fames ac. Vel risus commodo viverra maecenas accumsan lacus vel. Quam nulla porttitor massa id neque aliquam vestibulum morbi blandit. Etiam sit amet nisl purus in mollis.',
+      priority: 2,
+   },
+   {
+      src: './static/image2.jpg',
+      type: 'img',
+      title: 'Fantasy',
+      caption:
+         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      priority: 1,
+   },
+   {
+      src: './static/image3.jpg',
+      type: 'img',
+      title: 'Rainy City',
+      caption: 'Aesthetic view.',
+      priority: 1,
+   },
+   {
+      src: './static/birds.mp3',
+      type: 'audio',
+      title: 'Birds Chirping',
+      caption:
+         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      priority: 3,
+   },
+]
+
+const mixgallery = MixGallery(document.querySelector('#gallery'), elements)
+
+// mixgallery.destroy();
+// console.log("Private variable:", mixgallery._allElements);
+// log(document.querySelector(".mix-gallery"));
+// log(document.querySelector("body"));
--- public/assets/js/init.js
+++ public/assets/js/init.js
@@ -1,6 +1,67 @@
 //wow js initialization
 new WOW().init()
 
+const log = console.log
+const elements = [
+   {
+      src: 'assets/images/gallery/yeeji.jpg',
+      type: 'img',
+      title: 'Traceserv Team Building 2021',
+      caption:
+         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id aliquet lectus proin nibh. Diam quam nulla porttitor massa id neque aliquam vestibulum. Velit scelerisque in dictum non. In egestas erat imperdiet sed euismod nisi porta lorem. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Aliquet sagittis id consectetur purus ut. Eget est lorem ipsum dolor sit amet. Iaculis urna id volutpat lacus laoreet non. Enim sit amet venenatis urna. Commodo elit at imperdiet dui accumsan sit amet. Sagittis orci a scelerisque purus semper eget duis. Pharetra pharetra massa massa ultricies mi. Etiam sit amet nisl purus in mollis nunc sed id. Tristique senectus et netus et malesuada fames ac. Vel risus commodo viverra maecenas accumsan lacus vel. Quam nulla porttitor massa id neque aliquam vestibulum morbi blandit. Etiam sit amet nisl purus in mollis.',
+      priority: 1,
+   },
+   {
+      src: 'assets/images/gallery/bicol.jpg',
+      type: 'img',
+      title: 'Nice Landscape',
+      caption:
+         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id aliquet lectus proin nibh. Diam quam nulla porttitor massa id neque aliquam vestibulum. Velit scelerisque in dictum non. In egestas erat imperdiet sed euismod nisi porta lorem. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Aliquet sagittis id consectetur purus ut. Eget est lorem ipsum dolor sit amet. Iaculis urna id volutpat lacus laoreet non. Enim sit amet venenatis urna. Commodo elit at imperdiet dui accumsan sit amet. Sagittis orci a scelerisque purus semper eget duis. Pharetra pharetra massa massa ultricies mi. Etiam sit amet nisl purus in mollis nunc sed id. Tristique senectus et netus et malesuada fames ac. Vel risus commodo viverra maecenas accumsan lacus vel. Quam nulla porttitor massa id neque aliquam vestibulum morbi blandit. Etiam sit amet nisl purus in mollis.',
+      priority: 1,
+   },
+   {
+      src: 'assets/images/gallery/boracay.jpg',
+      type: 'img',
+      title: 'Nice Landscape',
+      caption:
+         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id aliquet lectus proin nibh. Diam quam nulla porttitor massa id neque aliquam vestibulum. Velit scelerisque in dictum non. In egestas erat imperdiet sed euismod nisi porta lorem. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Aliquet sagittis id consectetur purus ut. Eget est lorem ipsum dolor sit amet. Iaculis urna id volutpat lacus laoreet non. Enim sit amet venenatis urna. Commodo elit at imperdiet dui accumsan sit amet. Sagittis orci a scelerisque purus semper eget duis. Pharetra pharetra massa massa ultricies mi. Etiam sit amet nisl purus in mollis nunc sed id. Tristique senectus et netus et malesuada fames ac. Vel risus commodo viverra maecenas accumsan lacus vel. Quam nulla porttitor massa id neque aliquam vestibulum morbi blandit. Etiam sit amet nisl purus in mollis.',
+      priority: 1,
+   },
+   {
+      src: 'assets/images/gallery/Ryeji.jpg',
+      type: 'img',
+      title: 'Nice Landscape',
+      caption:
+         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id aliquet lectus proin nibh. Diam quam nulla porttitor massa id neque aliquam vestibulum. Velit scelerisque in dictum non. In egestas erat imperdiet sed euismod nisi porta lorem. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Aliquet sagittis id consectetur purus ut. Eget est lorem ipsum dolor sit amet. Iaculis urna id volutpat lacus laoreet non. Enim sit amet venenatis urna. Commodo elit at imperdiet dui accumsan sit amet. Sagittis orci a scelerisque purus semper eget duis. Pharetra pharetra massa massa ultricies mi. Etiam sit amet nisl purus in mollis nunc sed id. Tristique senectus et netus et malesuada fames ac. Vel risus commodo viverra maecenas accumsan lacus vel. Quam nulla porttitor massa id neque aliquam vestibulum morbi blandit. Etiam sit amet nisl purus in mollis.',
+      priority: 1,
+   },
+   {
+      src: 'assets/images/gallery/palawan.jpg',
+      type: 'img',
+      title: 'Nice Landscape',
+      caption:
+         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id aliquet lectus proin nibh. Diam quam nulla porttitor massa id neque aliquam vestibulum. Velit scelerisque in dictum non. In egestas erat imperdiet sed euismod nisi porta lorem. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Aliquet sagittis id consectetur purus ut. Eget est lorem ipsum dolor sit amet. Iaculis urna id volutpat lacus laoreet non. Enim sit amet venenatis urna. Commodo elit at imperdiet dui accumsan sit amet. Sagittis orci a scelerisque purus semper eget duis. Pharetra pharetra massa massa ultricies mi. Etiam sit amet nisl purus in mollis nunc sed id. Tristique senectus et netus et malesuada fames ac. Vel risus commodo viverra maecenas accumsan lacus vel. Quam nulla porttitor massa id neque aliquam vestibulum morbi blandit. Etiam sit amet nisl purus in mollis.',
+      priority: 1,
+   },
+   // {
+   //    src: '//',
+   //    type: 'img',
+   //    title: 'Fantasy',
+   //    caption:
+   //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+   //    priority: 1,
+   // },
+   // {
+   //    src: './static/image3.jpg',
+   //    type: 'img',
+   //    title: 'Rainy City',
+   //    caption: 'Aesthetic view.',
+   //    priority: 1,
+   // },
+]
+const mixgallery = MixGallery(document.querySelector('#gallery'), elements)
+mixgallery.render()
+
 $(document).ready(() => {
    /* Navbar About us*/
    $('.menu-item').on('click', () => {
@@ -19,6 +80,16 @@ $(document).ready(() => {
       $('#seeMore').removeClass('hidden')
    })
    /* About Us Page --> see less*/
+
+   /* Ajax gallery */
+   $.ajax({
+      url: 'assets/JSON/galleryJSON.json',
+      dataType: 'json',
+      success: (data) => {},
+      error: (e) => {},
+   })
+   /* Ajax gallery*/
+   /** services */
    $('#cardOne').on('click', () => {
       clickCardChange('#cardOne')
    })
