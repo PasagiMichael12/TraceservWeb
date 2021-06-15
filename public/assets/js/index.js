dd4a8ecb83b58fad7f613f85d22ed121a1909388
Maguszxc <Magtalasallon@gmail.com>
2021-06-15 09:11:38 +0800

    Traceserv website done

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -59,14 +59,6 @@ $(document).ready(() => {
    })
    /* About Us Page --> see less*/
 
-   /* Ajax gallery */
-   $.ajax({
-      url: 'assets/JSON/galleryJSON.json',
-      dataType: 'json',
-      success: (data) => {},
-      error: (e) => {},
-   })
-   /* Ajax gallery*/
    /** services */
    $('#cardOne').on('click', () => {
       clickCardChange('#cardOne')
@@ -121,49 +113,3 @@ $(document).ready(() => {
       $('#govCards').removeClass('hidden')
    })
 })
-
-const elements = [
-   {
-      src: 'assets/images/gallery/yeeji.jpg',
-      type: 'img',
-      title: 'Traceserv Team Building 2021',
-      caption:
-         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id aliquet lectus proin nibh. Diam quam nulla porttitor massa id neque aliquam vestibulum. Velit scelerisque in dictum non. In egestas erat imperdiet sed euismod nisi porta lorem. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Aliquet sagittis id consectetur purus ut. Eget est lorem ipsum dolor sit amet. Iaculis urna id volutpat lacus laoreet non. Enim sit amet venenatis urna. Commodo elit at imperdiet dui accumsan sit amet. Sagittis orci a scelerisque purus semper eget duis. Pharetra pharetra massa massa ultricies mi. Etiam sit amet nisl purus in mollis nunc sed id. Tristique senectus et netus et malesuada fames ac. Vel risus commodo viverra maecenas accumsan lacus vel. Quam nulla porttitor massa id neque aliquam vestibulum morbi blandit. Etiam sit amet nisl purus in mollis.',
-      priority: 1,
-   },
-   {
-      src: 'assets/images/gallery/bicol.jpg',
-      type: 'img',
-      title: 'Nice Landscape',
-      caption:
-         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id aliquet lectus proin nibh. Diam quam nulla porttitor massa id neque aliquam vestibulum. Velit scelerisque in dictum non. In egestas erat imperdiet sed euismod nisi porta lorem. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Aliquet sagittis id consectetur purus ut. Eget est lorem ipsum dolor sit amet. Iaculis urna id volutpat lacus laoreet non. Enim sit amet venenatis urna. Commodo elit at imperdiet dui accumsan sit amet. Sagittis orci a scelerisque purus semper eget duis. Pharetra pharetra massa massa ultricies mi. Etiam sit amet nisl purus in mollis nunc sed id. Tristique senectus et netus et malesuada fames ac. Vel risus commodo viverra maecenas accumsan lacus vel. Quam nulla porttitor massa id neque aliquam vestibulum morbi blandit. Etiam sit amet nisl purus in mollis.',
-      priority: 1,
-   },
-   {
-      src: 'assets/images/gallery/boracay.jpg',
-      type: 'img',
-      title: 'Nice Landscape',
-      caption:
-         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id aliquet lectus proin nibh. Diam quam nulla porttitor massa id neque aliquam vestibulum. Velit scelerisque in dictum non. In egestas erat imperdiet sed euismod nisi porta lorem. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Aliquet sagittis id consectetur purus ut. Eget est lorem ipsum dolor sit amet. Iaculis urna id volutpat lacus laoreet non. Enim sit amet venenatis urna. Commodo elit at imperdiet dui accumsan sit amet. Sagittis orci a scelerisque purus semper eget duis. Pharetra pharetra massa massa ultricies mi. Etiam sit amet nisl purus in mollis nunc sed id. Tristique senectus et netus et malesuada fames ac. Vel risus commodo viverra maecenas accumsan lacus vel. Quam nulla porttitor massa id neque aliquam vestibulum morbi blandit. Etiam sit amet nisl purus in mollis.',
-      priority: 1,
-   },
-   {
-      src: 'assets/images/gallery/Ryeji.jpg',
-      type: 'img',
-      title: 'Nice Landscape',
-      caption:
-         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id aliquet lectus proin nibh. Diam quam nulla porttitor massa id neque aliquam vestibulum. Velit scelerisque in dictum non. In egestas erat imperdiet sed euismod nisi porta lorem. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Aliquet sagittis id consectetur purus ut. Eget est lorem ipsum dolor sit amet. Iaculis urna id volutpat lacus laoreet non. Enim sit amet venenatis urna. Commodo elit at imperdiet dui accumsan sit amet. Sagittis orci a scelerisque purus semper eget duis. Pharetra pharetra massa massa ultricies mi. Etiam sit amet nisl purus in mollis nunc sed id. Tristique senectus et netus et malesuada fames ac. Vel risus commodo viverra maecenas accumsan lacus vel. Quam nulla porttitor massa id neque aliquam vestibulum morbi blandit. Etiam sit amet nisl purus in mollis.',
-      priority: 1,
-   },
-   {
-      src: 'assets/images/gallery/palawan.jpg',
-      type: 'img',
-      title: 'Nice Landscape',
-      caption:
-         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id aliquet lectus proin nibh. Diam quam nulla porttitor massa id neque aliquam vestibulum. Velit scelerisque in dictum non. In egestas erat imperdiet sed euismod nisi porta lorem. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Aliquet sagittis id consectetur purus ut. Eget est lorem ipsum dolor sit amet. Iaculis urna id volutpat lacus laoreet non. Enim sit amet venenatis urna. Commodo elit at imperdiet dui accumsan sit amet. Sagittis orci a scelerisque purus semper eget duis. Pharetra pharetra massa massa ultricies mi. Etiam sit amet nisl purus in mollis nunc sed id. Tristique senectus et netus et malesuada fames ac. Vel risus commodo viverra maecenas accumsan lacus vel. Quam nulla porttitor massa id neque aliquam vestibulum morbi blandit. Etiam sit amet nisl purus in mollis.',
-      priority: 1,
-   },
-]
-const mixgallery = MixGallery(document.querySelector('#gallery'), elements)
-
-mixgallery.render()
